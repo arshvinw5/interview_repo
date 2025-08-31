3625be745a0bde3d0766e58973b1cfa14c930891
arshvinw5 <tarshvin@gmail.com>
2025-08-31 13:40:48 +0530

    first commit

---
+++ ContextProvider/AuthContecxt.tsx
@@ -0,0 +1,20 @@
+import { Session } from '@supabase/supabase-js';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { supabase } from '~/utils/supabase';
+
+const AuthContext = createContext({});
+
+export default function AuthContextProvider({ children }: { children: React.ReactNode }) {
+  const [session, setSession] = useState<Session | null>(null);
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+  return <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>;
+}
+
+export const useAuth = () => useContext(AuthContext);
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import AuthContextProvider from '~/ContextProvider/AuthContecxt';
 import '../global.css';
 
 import { Stack } from 'expo-router';
@@ -9,9 +10,11 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-    </Stack>
+    <AuthContextProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+      </Stack>
+    </AuthContextProvider>
   );
 }
